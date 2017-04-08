3ea3bf064d95c96d804ea0e480b33c1d0365d530
armenqa <armenqa.stdev@gmail.com>
2017-04-08 13:21:58 +0400

    Steps

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -2,6 +2,8 @@ package aca.first.test;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,25 +14,37 @@ import static org.testng.AssertJUnit.assertEquals;
 Steps
 1: Opened "Mozilla Firefox".
 2: Enter "compensation.codebnb.me" in the browser field.
+3: Click on the button "SIGN IN"
+4: In opened window enter valid "ligin" and "password"
 Actual result: Should be redirect on the "http://compensation.codebnb.me/" page.
 Expected result: Redirected on the "http://compensation.codebnb.me/" page.
  */
 public class MyFirstWebDriverTest {
-	
+	//
+	//
 	@Test()
 	public void myFirstTest(){
+		// Opened "Mozilla Firefox"
 		 WebDriver driver = new FirefoxDriver();
-		// driver.get("http://aca.am");
+		// Open http://compensation.codebnb.me/ page
 		driver.get("http://compensation.codebnb.me/");
-		
-		//Assert.assertEquals("compensation", driver.getTitle(), "Compensation tool");
 
+		// Make sure that our site has an element by this xpath    //span[contains(.,'Compensation')]
 		assertEquals("COMPENSATION", driver.findElement(By.xpath("//span[contains(.,'Compensation')]")).getText());
 
+		// Make sure that our site has an element by this xpath    //a[@href='/accounts/login']"
 		assertEquals("SIGN IN", driver.findElement(By.xpath("//a[@href='/accounts/login']")).getText());
 
-		 driver.close();
-		 driver.quit();
+		// Click on the button "SIGN IN"
+		driver.findElement(By.xpath("//a[@href='/accounts/login']")).click();
+		//shoud be opened "ligin" and "password" popup window
+
+	/*	driver.findElement(By.xpath("//input[@id='id_username']")).sendKeys("dallas" );
+		driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("Password" );
+		driver.findElement(By.xpath("//input[@class='btn blockBtn blue']")).click(); */
+
+		 	 driver.close();
+		 	 driver.quit();
 	}
 
 }
