4de9092d2625615c85237c398010547fc223a7a3
armenqa <armenqa.stdev@gmail.com>
2017-04-14 12:25:18 +0400

    Test

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,27 +1,20 @@
 package aca.first.test;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.Iterator;
-import java.util.Set;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 public class MyFirstWebDriverTest {
+	Xpaths xpaths=new Xpaths();
 	private WebDriver driver;
 	/*
     Steps
@@ -44,17 +37,17 @@ public class MyFirstWebDriverTest {
 		// Make sure that our site has an element by this xpath    //a[@href='/accounts/login']"
 		assertEquals("SIGN IN", driver.findElement(By.xpath("//a[@href='/accounts/login']")).getText());
 		// Click on the button "SIGN IN"
-		driver.findElement(By.xpath("//a[@href='/accounts/login']")).click();
+		driver.findElement(xpaths.login).click();
 		//shoud be opened "login" and "password" popup window
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		// sendkey for the input field "username"
-		driver.findElement(By.xpath("//input[@id='id_username']")).sendKeys("armen" );
+		driver.findElement(xpaths.username).sendKeys("armen" );
 		// sendkey for the input field "password"
-		driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("Password" );
-		Thread.sleep(6000);
+		driver.findElement(xpaths.password).sendKeys("Password" );
+		Thread.sleep(3000);
 		//press submit
-		driver.findElement(By.xpath("//input[@class='btn blockBtn blue']")).click();
-		Thread.sleep(10000);
+		driver.findElement(xpaths.submit).click();
+		Thread.sleep(4000);
 
 	}
 	/*
@@ -70,18 +63,18 @@ public class MyFirstWebDriverTest {
 	@Test()
 	public void jobList() throws InterruptedException {
 		//Click on the "JOB LIST" button on main menu
-		driver.findElement(By.xpath("//a[text()='Job List']")).click();
-		Thread.sleep(10000);
+		driver.findElement(xpaths.jobList).click();
+		Thread.sleep(5000);
 		//Verify that we redirected on the page "Job List"
-		assertEquals("City of Houston: Job list", driver.findElement(By.xpath("//h6[text()='City of Houston: Job list']")).getText());
-		Thread.sleep(10000);
+		assertEquals("City of Houston: Job list", driver.findElement(xpaths.verifyJoblist).getText());
+		Thread.sleep(4000);
 		//Click on the dropdown item to "Find Job"
-		driver.findElement(By.xpath("//a[@data-original-title='Show All Items']")).click();
+		driver.findElement(xpaths.findJobDropdown).click();
 		Thread.sleep(6000);
 		//Click on the firs "Edit" icon of  list
-		driver.findElement(By.xpath(".//tbody/tr[1]/td[9]/a")).click();
+		driver.findElement(xpaths.firstEdit).click();
 		//Should be redirect on the "http://compensation.codebnb.me/jobs/edit/6675/" page
-		Thread.sleep(10000);
+		Thread.sleep(5000);
 		//Verify that we redirect on the "http://compensation.codebnb.me/jobs/edit/6675/" page
 
 	}
@@ -101,28 +94,28 @@ public class MyFirstWebDriverTest {
 	@Test()
 	public void employees() throws InterruptedException {
 
-        //navigate to the Surveys page
-        driver.findElement(By.xpath("//a[text()='Employees']")).click();
-        Thread.sleep(10000);
+        //navigate to the Employees page
+        driver.findElement(xpaths.employees).click();
+        Thread.sleep(4000);
         //click on the "Last name" icon
-        driver.findElement(By.xpath(".//*[@id='employee_table']/div/div[2]/table/thead/tr/th[2]/button")).click();
-        Thread.sleep(10000);
+        driver.findElement(xpaths.lastNameIcon).click();
+        Thread.sleep(4000);
         //click on the input field "Last name icon" and enter the "Tina"
-        driver.findElement(By.xpath(".//*[@id='first_name_panel']/div[1]/div[1]/input")).sendKeys("Tina" );
-        Thread.sleep(10000);
+        driver.findElement(xpaths.inpFild).sendKeys("Tina" );
+        Thread.sleep(4000);
         //click on the search icon  //should be shown only "Tyna" information in the list
-        driver.findElement(By.xpath(".//*[@id='first_name_panel']/div[1]/div[1]/button")).click();
-        Thread.sleep(10000);
+        driver.findElement(xpaths.searchbutton).click();
+        Thread.sleep(4000);
         //click on the search icon
-        driver.findElement(By.xpath(".//*[@id='employee_table']/div/div[2]/table/thead/tr/th[2]/button")).click();
-        Thread.sleep(10000);
+        driver.findElement(xpaths.searchIcon).click();
+        Thread.sleep(4000);
         //clear "Tena" in the input field
-        driver.findElement(By.xpath(".//*[@id='first_name_panel']/div[1]/div[1]/input")).clear();
+        driver.findElement(xpaths.clearTina).clear();
         //click on the search icon  //should be shown all information in the list
-        driver.findElement(By.xpath(".//*[@id='first_name_panel']/div[1]/div[1]/button")).click();
-        Thread.sleep(10000);
+        driver.findElement(xpaths.searchbutton).click();
+        Thread.sleep(4000);
         //navigate second page of "Employees"
-        driver.findElement(By.xpath(".//html/body/div/div[3]/ul/li[4]/a")).click();
+        driver.findElement(xpaths.navigateSecond).click();
 	}
 	/*
 		Preconditions: When we logged in
@@ -137,16 +130,16 @@ public class MyFirstWebDriverTest {
 	@Test()
 	public void surveys() throws InterruptedException {
 		//Click on the "Surveys" button on main menu
-		driver.findElement(By.xpath("//a[text()='Surveys']")).click();
-		Thread.sleep(10000);
+		driver.findElement(xpaths.surveys).click();
+		Thread.sleep(4000);
 		//Click on the first row of the "Survey Data" list
-		driver.findElement(By.xpath(".//tbody/tr[1 and @class='survey_job_code active'] ")).click();
+		driver.findElement(xpaths.surveyData).click();
 		Thread.sleep(3000);
 		//Click on the drop down icon of the "Details"    http://joxi.ru/bmoJ9jecZzKGry
-		driver.findElement(By.xpath(".//*[@id='survey_types']")).click();
+		driver.findElement(xpaths.detailsIcon).click();
 		Thread.sleep(3000);
 		//Click on the fourth element of the drop down list of "Details"
-		driver.findElement(By.xpath(".//*[@id='survey_types']/option[4]")).click();
+		driver.findElement(xpaths.detailsElement).click();
 		//Our "Base,TTC,TDC" information should be updated
 	}
 
@@ -163,14 +156,12 @@ public class MyFirstWebDriverTest {
 	@Test()
 	public void marketMatcher() throws InterruptedException {
 		//Click on the "Market Matcher" button on main menu
-		driver.findElement(By.xpath("//a[text()='Market Matcher']")).click();
-        Thread.sleep(10000);
-		driver.findElement(By.xpath("//a[@class='custom-combobox-toggle ui-corner-right']")).click();
-		Thread.sleep(10000);
-        driver.findElement(By.xpath("//li[text()='Engineer']")).click();
-        Thread.sleep(10000);
-
-
+		driver.findElement(xpaths.marketMatcher).click();
+        Thread.sleep(4000);
+		driver.findElement(xpaths.comboBox).click();
+		Thread.sleep(4000);
+        driver.findElement(xpaths.liEngineer).click();
+        Thread.sleep(4000);
 	}
 
 		/*
@@ -186,11 +177,11 @@ public class MyFirstWebDriverTest {
 
 		Actions builder = new Actions(driver);
 		//Hover on the "Reports" button on main menu
-		builder.moveToElement(driver.findElement(By.xpath("//a[@href='#']"))).perform();
-		Thread.sleep(5000);
+		builder.moveToElement(driver.findElement(xpaths.reports)).perform();
+		Thread.sleep(2000);
 		// Click on the "Jobs Reports" from the "Reports" dropdown menu
-		builder.moveToElement(driver.findElement(By.xpath("//a[contains(.,'Jobs Reports')]"))).click();
-		Thread.sleep(10000);
+		builder.moveToElement(driver.findElement(xpaths.jobReports)).click();
+		Thread.sleep(2000);
 		//driver.findElement(By.xpath("//button[@class='fadeToggleBtn']")).click();
 
 //		driver.findElement(By.xpath(".//*[@id='employee_table']/div/div[2]/table/thead/tr/th[2]/button")).click();
